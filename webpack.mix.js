d5599f0a6d0380146bc24df0975821e7cbde5a45
Soroosh <integralproject1988@gmail.com>
2021-01-02 17:33:07 +0330

    ok

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,35 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+//css bundle
+mix.combine([
+    'resources/plugins/bootstrap/bootstrap.min.css',
+    'resources/plugins/slick/slick.css',
+    'resources/plugins/venobox/venobox.css',
+    'resources/plugins/animate/animate.css',
+    'resources/plugins/aos/aos.css',
+    'resources/plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.min.css',
+    'resources/plugins/nice-select/nice-select.css',
+    'resources/plugins/bootstrap-slider/bootstrap-slider.min.css',
+    'resources/plugins/fontawesome-free-5.15.1-web/css/all.css',
+    'resources/css/style.css'
+], 'public/css/app.css');
+
+/* mix.combine(['resources/css/style.css'], 'public/css/app.css');
+ */
+//js bundle
+mix.combine([
+    "resources/plugins/jQuery/jquery.min.js",
+    "resources/plugins/bootstrap/bootstrap.min.js",
+    "resources/plugins/slick/slick.min.js",
+    "resources/plugins/venobox/venobox.min.js",
+    "resources/plugins/aos/aos.js",
+    "resources/plugins/syotimer/jquery.syotimer.js",
+    "resources/plugins/instafeed/instafeed.min.js",
+    "resources/plugins/zoom-master/jquery.zoom.min.js",
+    "resources/plugins/bootstrap-touchspin-master/jquery.bootstrap-touchspin.min.js",
+    "resources/plugins/nice-select/jquery.nice-select.min.js",
+    "resources/plugins/bootstrap-slider/bootstrap-slider.min.js",
+    "resources/plugins/google-map/gmap.js",
+    "resources/js/script.js",
+], 'public/js/app.js');
