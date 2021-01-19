698a18e05b8f9198020b78b3cf3db107920b8c2a
Soroosh <integralproject1988@gmail.com>
2021-01-19 12:46:05 +0330

    ok

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,11 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-
-
+/*     public function __construct()
+    {
+        $this->middleware(['verified']);
+    }
+ */
     public function index()
     {
         return view('auth.user-profile');
